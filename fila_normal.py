69778173548cfb6ea8390ee12c1b5b483a159b4d
André Arruda <andre23arruda@gmail.com>
2020-08-22 13:58:11 -0300

    aula 1-3

--- fila_normal.py
+++ fila_normal.py
@@ -7,3 +7,19 @@ class filanormal:
 
     def gerasenhaatual(self)->None:
         self.senha_atual = f'NM{self.codigo}'
+
+    def resetafila(self)->None:
+        if self.codigo >= 100:
+            self.codigo = 100
+        else:
+            self.codigo += 1
+
+    def atualizafila(self)->None:
+        self.resetafila()
+        self.gerasenhaatual()
+        self.fila.append(self.senha_atual)
+
+    def chamacliente(self, caixa:int)->str:
+        cliente_atual:str = self.fila.pop(0)
+        self.clientes_atendidos.append(cliente_atual)
+        return f'Cliente atual: {cliente_atual}, dirija-se ao caixa {caixa}'
