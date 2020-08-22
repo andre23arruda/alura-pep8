5f830b463c76d4ca7c9be5285bf517b4936c8e90
André Arruda <andre23arruda@gmail.com>
2020-08-22 15:04:00 -0300

    aula 1-4

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fila_normal import filanormal
+from fila_prioritaria import Fila_Prioritaria
 
 fila_teste = filanormal()
 fila_teste.atualizafila()
@@ -7,3 +8,12 @@ fila_teste.atualizafila()
 fila_teste.atualizafila()
 print(fila_teste.chamacliente(5))
 print(fila_teste.chamacliente(10))
+
+fila_teste = Fila_Prioritaria()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+print(fila_teste.chama_cliente(5))
+print(fila_teste.chama_cliente(10))
+print(fila_teste.estatistica('12/05/1632', 1, 'detail'))
